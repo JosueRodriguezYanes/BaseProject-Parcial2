1d4757143b14b104b09bd9e15b1e7121a4e4a785
Aleejandrag <168794015+Aleejandrag@users.noreply.github.com>
2025-05-06 18:14:47 -0600

    cambios

--- app/Http/Controllers/MonedaController.php
+++ app/Http/Controllers/MonedaController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class MonedaController extends Controller
 {
-    //
+    public function convertir(Request $request)
+    {
+        $client = new \SoapClient("https://currencyconverter.kowabunga.net/converter.asmx?WSDL");
+
+        $params = [
+            'CurrencyFrom' => $request->from,
+            'CurrencyTo' => $request->to,
+            'RateDate' => $request->date,
+        ];
+
+        try {
+            $rateResponse = $client->GetConversionRate($params);
+            $rate = $rateResponse->GetConversionRateResult;
+            $resultado = $rate * floatval($request->amount);
+
+            return redirect()->back()->with('resultado', round($resultado, 2));
+        } catch (\Exception $e) {
+            return redirect()->back()->with('resultado', 'Error: ' . $e->getMessage());
+        }
+    }
 }
